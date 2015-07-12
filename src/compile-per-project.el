bb0807ffa80ac565b5c673fdd1ced1161a03677c
Vasiliy Kevroletin <kevroletin@gmail.com>
2015-07-12 12:51:31 +1000

    Persist compilation commands cache between sessions
    After start of new session ask user for directory and command but show
    advice with values from cache.
    
    Implementation details:
    + Use IEIEO objects instad of list with 2 values.
    + Add already-executed flag to each command.
    + Reuse projectile's serialization/deserialization functions to store
    cache into file.

--- src/compile-per-project.el
+++ src/compile-per-project.el
@@ -2,20 +2,66 @@
 
 ;;; Commentary:
 
-; TODO:
-; + code will throw without loaded projectile
-; + save cache to disk
-; + clear loaded cache
-; + organize test properly
-
+;;; TODO:
+;; Implementation:
+;; + code will throw without loaded projectile
+;; + move hook change into configuration files
+;; + after loading cache ask user during first compilation
+;; + fix naming
+;; + flush cache from time to time to disk
+;; Testing:
+;; + use interfaces instead of dynamic scoping to make test environment
+;; + organize tests properly
 ;;; Code:
 
 (require 'compile)
 
-(defvar my:compile-cmd-map (make-hash-table :test 'equal))
+;;; Customization
+(defgroup compile-per-project nil
+  "Improved compile command."
+  :group 'tools
+  :group 'convenience)
+
+(defcustom cache-file
+  (expand-file-name "compile-per-project.cache" user-emacs-directory)
+  "The name of cache file."
+  :group 'compile-per-project
+  :type 'string)
+
+
+;;; Implementation
+(defclass compilation-command ()
+  ((directory :documentation "Compilation directory"
+              :initarg :directory
+              :type string
+              :accessor get-directory)
+   (command :documentation "Compilation command"
+            :initarg :command
+            :type string
+            :accessor get-command)
+   (already-executed :documentation "Were command executed
+during current session or no"
+                     :type boolean
+                     :initform nil
+                     :initarg :already-executed
+                     :accessor is-already-executed
+                     :writer set-already-executed))
+  "Data required to repeat build command.")
+
+(defvar my:compile-cmd-map )
 
 (defun my:init-compile-cmd-map()
   (setq my:compile-cmd-map (make-hash-table :test 'equal)))
+
+;; reuse projectile-unserialize
+(defun my:load-cache-file ()
+  (setq my:compile-cmd-map
+        (projectile-unserialize cache-file))
+  (maphash (lambda (k v) (set-already-executed v nil))
+           my:compile-cmd-map))
+
+(defun my:store-cache-into-file ()
+  (projectile-serialize my:compile-cmd-map cache-file))
 
 (defun my:project-dir()
   "Determines project root using Projectile package and returns absolute path.
@@ -26,33 +72,44 @@ Returns nil of there is no project."
       (error ()))))
 
 (defun my:recompile(compile-obj)
-  (let ((compilation-directory (car compile-obj))
-        (command (nth 1 compile-obj)))
-    (compilation-start command)))
+  (let ((default-directory (get-directory compile-obj))
+        (command (get-command compile-obj)))
+    (compilation-start command))
+  compile-obj)
 
 (defun my:ask-and-recompile(last-compile-obj)
-  (let* ((dir (read-directory-name "Compile in: "
-                                   (or (nth 0 last-compile-obj)
-                                       compilation-directory
-                                       default-directory)))
-         (prompt-cmd (or (nth 1 last-compile-obj)
+  (let* ((advice-dir (or (and last-compile-obj (get-directory last-compile-obj))
+                         compilation-directory
+                         default-directory))
+         (advice-cmd (or (and last-compile-obj (get-command last-compile-obj))
                          (eval compile-command))) ; eval is from recompile definition
-         (cmd (compilation-read-command prompt-cmd))
-         (new-compile-cmd (list dir cmd)))
-    (puthash (my:project-dir) new-compile-cmd  my:compile-cmd-map)
-    (my:recompile new-compile-cmd)))
+         (dir (read-directory-name "Compile in: " advice-dir))
+         (cmd (compilation-read-command advice-cmd))
+         (new-compile-obj (compilation-command "compilation-command"
+                                               :directory dir :command cmd)))
+    (my:recompile new-compile-obj)
+    new-compile-obj))
+
+(defun my:cache-executed-command (project-id compile-obj)
+  (puthash project-id
+           (clone compile-obj :already-executed t)
+           my:compile-cmd-map))
 
 (defun compile-per-project(&optional arg)
   "During first call (or with prefix argument) asks compilation
 directory and command. All successive calls without prefix argument cause
 recompilation"
   (interactive "P")
-  (let ((last-compile-obj (gethash (my:project-dir) my:compile-cmd-map))
-        (force-dialog (consp arg)))
-    (if (and last-compile-obj
-             (not force-dialog))
-        (my:recompile last-compile-obj)
-      (my:ask-and-recompile last-compile-obj))))
+  (let* ((project-id (my:project-dir))
+         (last-compile-obj (gethash  project-id my:compile-cmd-map))
+         (is-force-dialog (consp arg)))
+    (my:cache-executed-command
+     project-id
+     (if (and last-compile-obj
+              (is-already-executed last-compile-obj)
+              (not is-force-dialog))
+         (my:recompile last-compile-obj)
+       (my:ask-and-recompile last-compile-obj)))))
 
 ;;; Tests
 
@@ -87,29 +144,37 @@ recompilation"
           (fset fname old-fun-body))))))
 
 (ert-deftest my:test-recompile()
-  "Ensure that my:recompile configures compilation-directory global variable
+  "Ensure that my:recompile configures default-directory global variable
 and calls compilation-start with right argument."
   (let ((compilation-directory))
     (with-new-function
      'compilation-start
      (lambda (command)
-       (should (equal compilation-directory "/123/456"))
+       (should (equal default-directory "/123/456"))
        (should (equal command "mmmega command")))
      (lambda ()
-       (my:recompile (list "/123/456" "mmmega command"))))))
+       (my:recompile (compilation-command "test"
+                                          :directory "/123/456"
+                                          :command "mmmega command"))))))
 
 (ert-deftest my:test-ask-and-recompile ()
-  "test-ask-and-recompile should add new compilation command to map"
-  (let ((my:compile-cmd-map))
+  "test-ask-and-recompile should not modify compilation command map"
+  (let ((my:compile-cmd-map)
+        (compile-called nil))
     (with-new-function
      '((read-directory-name (lambda (x y) "/123/456"))
        (compilation-read-command (lambda (x) "mmmega command"))
-       (my:recompile (lambda (x)))
+       (my:recompile (lambda (x) (setq compile-called t)))
        (my:project-dir (lambda () "/project/dir")))
      (lambda ()
        (my:init-compile-cmd-map)
-       (my:ask-and-recompile nil)
-       (should (gethash "/project/dir" my:compile-cmd-map))))))
+       (should (compilation-command-p (my:ask-and-recompile nil)))
+       (should (equal (hash-table-count my:compile-cmd-map) 0))))))
+
+
+;; TODO: move hook change into configuration files
+(my:load-cache-file)
+(add-to-list 'kill-emacs-hook 'my:store-cache-into-file)
 
 (provide 'compile-per-project)
 ;;; scratch.el ends here
