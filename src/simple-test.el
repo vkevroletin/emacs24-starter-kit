143ec98fc81e36c07b621b7ce4c558eb8eb29760
Vasiliy Kevroletin <kevroletin@gmail.com>
2015-07-15 23:54:15 +1000

    Make simple testing framework simplier to use
    1. No macro no pain. Ert cause hard to understand problems with should
    macro. Several symbols to type lambda is not big dial. Simplicity and
    stability is best choise.
    
    2. Directly execte tests with C-M-x. Between test init/run calls
    simple-test function registers test for execution. After run it
    imidiatly executes code. So just C-M-x during regular editing seeeion to
    execute test.

--- src/simple-test.el
+++ src/simple-test.el
@@ -1,27 +1,46 @@
 ;; Simple test runner
-;; TODO:
-;; + turn simple-test and expect into macro
+;;
+;; No macro. No pain. Jump to failed test and directly execute with C-M-x.
+;;
 
 (defvar simple-tests (list))
 
+(defvar simple-tests-in-progress nil)
+
 (defun simple-test-init ()
+  (setq simple-tests-in-progress t)
   (setq simple-tests (list)))
 
 (defun simple-test (name body)
-  (push (cons name body) simple-tests))
+  (if simple-tests-in-progress
+      (push (cons name body) simple-tests)
+    (let ((debug-on-error t)
+          (debug-on-signal t)
+          (debug-on-quit t)))
+    (funcall body)))
 
-(defun expect (name body)
+(defun check (name body)
   (condition-case err
       (unless (funcall body)
         (error "result is nil"))
     (error
-     (error (format "expectation %s failed: %s" name (nth 1 err))))))
+     (error (format "check '%s' failed: %s" name (nth 1 err))))))
+
+(defun check-error (name body)
+  (condition-case err
+      (progn
+        (funcall body)
+        (error (format "check-error '%s' failed: code didn't throw error" name)))
+    (error
+     t)))
 
 (defun simple-tests-run ()
+  (when (not simple-tests-in-progress)
+    (error "1) call simple-test-init 2) define tests 3) call simple-tests-run"))
   (let ((ok-cnt 0)
         (fail-cnt 0))
     (dolist (test (reverse simple-tests))
-      (message ">> Execute test %s:" (car test))
+      (message ">> Execute test '%s':" (car test))
       (condition-case err
           (progn
             (funcall (cdr test))
@@ -29,21 +48,10 @@
             (message "   ok"))
         (error
          (setq fail-cnt (+ 1 fail-cnt))
-         (message (format "   test %s failed:\n   %s" (car test) (nth 1 err))))))
-    (message "Tests summary:")
-    (message "           ok: %d" ok-cnt)
-    (message "       failed: %d" fail-cnt)
-    (format "ok: %d; failed %d" ok-cnt fail-cnt)))
-
-
-(simple-test-init)
-
-(simple-test "123" (lambda ()
-                     (expect "true" (lambda () t))))
-
-(simple-test "fail" (lambda ()
-                     (expect "true" (lambda () nil))))
-
-(simple-tests-run)
+         (message (format "   test '%s' failed:\n   %s" (car test) (nth 1 err))))))
+    (let ((result (format  "Tests summary: ok %d, failed %d" ok-cnt fail-cnt)))
+      (message result)
+      result))
+  (setq simple-tests-in-progress nil))
 
 (provide 'simple-test)
