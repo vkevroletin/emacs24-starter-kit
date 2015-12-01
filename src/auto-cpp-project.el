7729128a61410a5693a9f8894f6696d2e7a719ac
Vasiliy Kevroletin <kevroletin@gmail.com>
2015-12-01 23:58:43 +1000

    Implement simple weight function for auto-cpp-proj
    Weigh function prefers:
    1. path which contains directories "inc", "include";
    2. path which contains directories "lib", "sdk" (but less than previous
    point);
    3. path which contains less directories (this factor affects much less than
    previous two).

--- src/auto-cpp-project.el
+++ src/auto-cpp-project.el
@@ -2,14 +2,43 @@
 (require 'dash)
 (require 'ede)
 
-;; TODO: create weigh function which considers length of path and
-;;       amount of names like "inc", "include", "sdk", "lib" etc
-(defun auto-cpp--best-match (dir file-name)
-  (let* ((default-directory dir)
-         (all-files  (projectile-current-project-files))
-         (good-files (--filter (string-match file-name it) all-files)))
-    (when (not (null good-files))
-      (expand-file-name (car good-files) dir))))
+(defun auto-cpp--substr-cnt (regex string)
+  (let ((start 0)
+        (res   0))
+    (while (string-match regex string start)
+      (setq res (+ 1 res))
+      (setq start (match-end 0)))
+    res))
+
+(defun auto-cpp--weight-function (path)
+  (let ((main-const
+         (+ (* 2 (auto-cpp--substr-cnt "/inc/" path))
+            (* 2 (auto-cpp--substr-cnt "/include/" path))
+            (auto-cpp--substr-cnt "lib" path)
+            (auto-cpp--substr-cnt "sdk" path)))
+        (path-len (auto-cpp--substr-cnt "/" path)))
+    (- (* 100 main-const)
+       path-len)))
+
+(defun auto-cpp--max-index (fun xs)
+  (unless (null xs)
+    (let ((best-val (funcall fun (car xs)))
+          (best-idx 0)
+          (rest     (cdr xs))
+          (idx      1))
+      (while (not (null rest))
+        (let ((val (funcall fun (car rest))))
+          (when (> val best-val)
+            (setq best-val val)
+            (setq best-idx idx)))
+        (setq rest (cdr rest))
+        (setq idx (+ 1 idx)))
+      best-idx)))
+
+(defun auto-cpp--choose-with-weight-function (xs)
+  (let* ((dirs (-map 'file-name-directory xs))
+         (res-idx (auto-cpp--max-index 'auto-cpp--weight-function dirs)))
+    (nth res-idx xs)))
 
 (defun auto-cpp--is-cpp-file (file)
   (let ((ext (file-name-extension file)))
@@ -24,8 +53,13 @@
     (or (> cpp-files-cnt 100)
         (> cpp-percents  20))))
 
-(defun auto-cpp--locate (filename root-dir)
-  (auto-cpp--best-match root-dir filename))
+(defun auto-cpp--locate (file-name root-dir)
+  (let* ((default-directory root-dir)
+         (all-files  (projectile-current-project-files))
+         (good-files (--filter (string-match file-name it) all-files)))
+    (when (not (null good-files))
+      (expand-file-name (auto-cpp--choose-with-weight-function good-files)
+                        root-dir))))
 
 (defun auto-cpp--file-for-dir (&optional dir)
   (let* ((default-directory dir)
@@ -35,6 +69,7 @@
              (proj-file-exists (file-exists-p proj-file-name)))
         (when (or proj-file-exists
                   (auto-cpp--guess-cpp-proj dir))
+          ;; TODO: create file in auto-cpp--load
           (when (not proj-file-exists)
             (with-temp-buffer (write-file proj-file-name)))
           proj-file-name)))))
