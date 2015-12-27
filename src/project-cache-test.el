cae1760f0cc47804ceb5cf80654f0ca31ff47fef
Vasiliy Kevroletin <kevroletin@gmail.com>
2015-12-27 15:09:31 +1000

    Factor out values storing from compile-per-project
    Functions which attach values to projectile projects and persists them
    between sessions are useful for other logic like configuring flycheck
    include paths without .dir-locals.el.

---
+++ src/project-cache-test.el
@@ -0,0 +1,51 @@
+;;; -*- lexical-binding: t -
+(package-initialize)
+(require 'f)
+(add-to-list 'load-path (f-dirname (f-this-file)))
+
+(require 'project-cache)
+
+(defun project-cache-test-init ()
+  (let ((tmp-file (concat temporary-file-directory "/project-cache-test.cache")))
+    (setq project-cache-file tmp-file)
+    (project-cache-clear)
+    (when (f-exists? tmp-file) (f-delete tmp-file))))
+
+(ert-deftest project-cache-test--multiple-put-get ()
+  (project-cache-test-init)
+
+  (let ((values '(("a/b/c" . 10)
+                  ("123" . 20)
+                  ("/" . 30))))
+    (dolist (x values)
+      (project-cache-put (car x) (cdr x)))
+    (dolist (x values)
+      (should (equal (project-cache-get (car x))
+                     (cdr x))))))
+
+(ert-deftest project-cache-test--put-get-nil ()
+  (project-cache-test-init)
+
+  (project-cache-put '() 10)
+  (should (null (project-cache-get '()))))
+
+(ert-deftest project-cache-test--put-get-nil ()
+  (project-cache-test-init)
+
+  (project-cache-put '() 10)
+  (should (null (project-cache-get '()))))
+
+(ert-deftest project-cache-test--file-write-read ()
+  (project-cache-test-init)
+
+  (should (null (project-cache-get "a")))
+  (project-cache-put "a" 10)
+  (should (equal (project-cache-get "a") 10))
+  (project-cache-write-to-file)
+  (should (equal (project-cache-get "a") 10))
+  (project-cache-clear)
+  (should (null (project-cache-get "a")))
+  (project-cache-read-from-file)
+  (should (equal (project-cache-get "a") 10)))
+
+(provide 'project-cache-test)
